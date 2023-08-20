3a20cfe170e86c892aaee17537f3a2b471ca5144
Sam Friedman <samueljacobfriedman@gmail.com>
2023-08-19 22:04:02 -0400

    fixed mistakes

--- apply_historical.py
+++ apply_historical.py
@@ -45,7 +45,7 @@ def get_data(symbol : str, api_key : str, redirect_url : str):
     return stock_data['open'].sort_index(axis=0).to_frame(name = 'price')
 
 #determines if stock moves pct(movement) up or down
-def sell_at(start, stock_data : pd.DataFrame, close_situation : str, close_at_end_day : bool, indicate = 1):
+def close_at(start, stock_data : pd.DataFrame, close_situation : str, close_at_end_day : bool):
     #if you want to close at end day get only prices on this date
     if close_at_end_day:
         price = stock_data.loc[stock_data.index.date == start.date()]
@@ -58,64 +58,71 @@ def sell_at(start, stock_data : pd.DataFrame, close_situation : str, close_at_en
     
     #returns pct change of trade if there is one
     if not movement.empty:
-        return indicate * ((movement.iloc[0] - price.iloc[0]) / price.iloc[0])
+        return pd.Series({'close_time' : movement.index[0], 'pct_change' : ((movement.iloc[0] - price.iloc[0]) / price.iloc[0])})
     
-    #returns none if err
-    return np.NaN
+    #returns sale if closed and close is true
+    return  pd.Series({'close_time' : price.index[-1], 'pct_change' : ((price.iloc[-1] - price.iloc[0]) / price.iloc[0])})
 
 #tests success rate of each "situation"
 def test_situation(situation, test_data : pd.DataFrame, stock_data : pd.DataFrame):
+    #iniates trades df
+    trades = pd.DataFrame(columns=['open_time', 'close_time', 'pct_change'])
+
     #gets the datetime occurences of each "situation" 
-    occurences = pd.Series(test_data.query(situation['open_situation']).index)
+    trades['open_time'] = pd.Series(test_data.query(situation['open_situation']).index)
+    # trades = pd.DataFrame(test_data.query(situation['open_situation']).index, columns=['open_time'])
 
     #changes the trades to the direction of the indicate
-    trades = occurences.apply(sell_at, args=(stock_data, situation['close_situation'], situation['close_at_end_of_day'], situation['indicate'])).dropna()
-
-
-    #returns accuracies
-    return pd.Series({'test_occurences': len(trades), 'test_accuracy':  len(trades[trades==situation['indicate']]) / len(trades)})
+    trades[['close_time', 'pct_change']] = trades['open_time'].apply(close_at, args=(stock_data, situation['close_situation'], situation['close_at_end_of_day']))
+    
+    #returns accuracy
+    return pd.Series({'test_occurences' : len(trades), 'profit': trades['pct_change'].sum()})
 
 #determines if the situation indicates to buy/sell/dont know
 def determine_indication(train_data : pd.DataFrame, stock_data: pd.DataFrame, situation : str, close_situation : str, close_at_end_day : bool):
+    #iniates trades df
+    trades = pd.DataFrame(columns=['open_time', 'close_time', 'pct_change'])
+
     #gets the datetime occurences of each "situation"
-    occurences = pd.Series(train_data.query(situation).index)
+    trades['open_time'] = pd.Series(train_data.query(situation).index)
 
     #performs "trades"
-    trades = occurences.apply(sell_at, args=(stock_data, close_situation, close_at_end_day)).dropna()
+    trades[['close_time', 'pct_change']] = trades['open_time'].apply(close_at, args=(stock_data, close_situation, close_at_end_day))
+
 
     #calculates confidence interval
-    confidence_interval = stats.t.interval(0.99, len(trades)-1, loc=np.mean(trades), scale=np.std(trades)/np.sqrt(len(trades)))
+    confidence_interval = stats.t.interval(0.99, len(trades['pct_change'])-1, loc=np.mean(trades['pct_change']), scale=np.std(trades['pct_change'])/np.sqrt(len(trades['pct_change'])))
     
     #determines if both values in the confidence interval are the same sign(aka does this have an acutal indication)
     sign = (np.sign(confidence_interval[0]) + np.sign(confidence_interval[1])) / 2
 
     #returns a dict(gonna be appended to a df)
-    if len(trades) != 0:
+    if not trades.empty:
         return {'open_situation' : situation,
                 'close_situation' : close_situation,
                 'close_at_end_of_day' : close_at_end_day,
                 'indicate' : sign,
                 'interval' : confidence_interval,
                 'train_occurences' : len(trades),
-                'train_accuracy' : max(len(trades[trades>0]) / len(trades), len(trades[trades<0]) / len(trades))}
+                'profit' : sign * trades['pct_change'].sum()}
     
+    #if nada
     return {'open_situation' : situation, 
             'close_situation' : close_situation,
             'close_at_end_of_day' : close_at_end_day,
             'indicate' : 0,
             'interval' : [0,0],
             'train_occurences' : 0,
-            'train_accuracy' : 0}
+            'profit' : 0}
 
 def trade(test_data : pd.DataFrame, stock_data : pd.DataFrame, situations : pd.DataFrame):
     #determines success rate of each "situation" on test_data
     if not situations.empty:
-        situations[['test_occurences', 'test_accuracy']] = situations.apply(test_situation, args=(test_data, stock_data), axis=1)
+        situations[['test_occurences', 'profit']] = situations.apply(test_situation, args=(test_data, stock_data), axis=1)
 
-        situations['growth'] = (situations['test_occurences'] * (situations['test_accuracy'] - (1-situations['test_accuracy'])))
-        
         #outputs that jawn
         return situations
+    
     return 'u got no data'
 
 
@@ -126,13 +133,14 @@ if __name__ == "__main__":
     max_time = '16:00'
     test_to_train_ratio = .5
 
+
     #get data from .env file
     load_dotenv()
     api_key = os.getenv("alpha_api_key")
     redirect_url = os.getenv('alpha_redirect_url')
 
     #initation
-    situations = pd.DataFrame(columns=['situation', 'indicate', 'train_occurences', 'train_accuracy'])
+    situations = pd.DataFrame(columns=['situation', 'indicate', 'train_occurences', 'profit'])
 
     # gets stock data
     # stock_data_1 = get_data(symbol, api_key, redirect_url)
@@ -156,7 +164,7 @@ if __name__ == "__main__":
     test_data = stock_data[(stock_data.index.month == 2)]
   
     # tests these indicators and appends that indication to df
-    # first string is your buy query, second string is your sell query
+    # paramaters in order are train data, all data, buy query, sell query, sell at end of day boolean
     situations = situations.append(determine_indication(train_data, stock_data, 'price / price.shift(60) >= 1.0025', '(price >= price.iloc[0] * (1 + 0.0025)) | (price <= price.iloc[0] * (1 - 0.0025))', True), ignore_index = True).drop('situation', axis=1)
 
     print(situations)
